1299c62b765804a1d9881d7147e9dcf54ad7cc60
Four Two Eight <hiim11354428@gmail.com>
2019-09-10 18:09:44 -0400

    Added String->Block thing, score/time counters, and energy system (alpha)
    also i'm using my bot account again

--- code.js
+++ code.js
@@ -7,21 +7,87 @@ var movesQueue = [[0], [1], [2], [3]];
 var players = {};
 var ratelimit = 5000;
 
-var tiles = {blank: Array(3).fill([87, 87, 87])}; // blank tile, solid white
+var tiles = {blank: Array(3).fill("¯¯¯")}; // blank tile, solid white
 
-tiles.fruit = [[ 87, 1074,   87],
+/*tiles.fruit = [[ 87, 1074,   87],
               [1074, 1074, 1074], // fruit
-               [ 87, 1074,   87]];
+               [ 87, 1074,   87]];*/
+tiles.fruit = ["¯,¯",
+               ",,,",
+               "¯,¯"];
+               
+// old fruit tile:
+// wђw
+// ђђђ
+// wђw
 
-tiles.snakeBody = Array(3).fill([14, 14, 14]); // snake body, solid green
+tiles.snakeBody = [Array(3).fill("jjj"), // snake body, solid green
+                   Array(3).fill("VVV")]; // snake body, charged, solid cyan
 
-tiles.snakeHeadX = [[14, 33, 14],
-                    [14, 14, 14], // moving left/right
-                    [14, 33, 14]];
+tiles.snakeHeadX = [["jAj",
+                     "jjj", // moving left/right
+                     "jAj"],
+                     
+                    ["VAV",
+                     "VVV", // moving left/right, charged
+                     "VAV"]];
 
-tiles.snakeHeadY = [[14, 14, 14],
-                    [33, 14, 33], // moving up/down
-                    [14, 14, 14]];
+
+
+tiles.snakeHeadY = [["jjj",
+                     "AjA", // moving up/down
+                     "jjj"],
+                     
+                    ["VVV",
+                     "AVA", // moving up/down, charged
+                     "VVV"]];
+
+tiles.numbers = [["¯¯¯",
+                  "¯ ¯", //0
+                  "¯¯¯"],
+                  
+                 ["SS ",
+                  " S ", //1
+                  "SSS"],
+                  
+                 ["ZZ ",
+                  " Z ", //2
+                  " ZZ"],
+                  
+                 ["YYY",
+                  " YY", //3
+                  "YYY"],
+                  
+                 ["k k",
+                  "kkk", //4
+                  "  k"],
+                  
+                 [" XX",
+                  " X ", //5
+                  "XX "],
+                  
+                 ["W  ",
+                  "WWW", //6
+                  "WWW"],
+                  
+                 ["VVV",
+                  "  V", //7
+                  "  V"],
+                  
+                 [" UU",
+                  "UUU", //8
+                  "UUU"],
+                  
+                 ["TTT",
+                  "TTT", //9
+                  "  T"],
+                  
+                 ["   ",
+                  "   ", //none
+                  "   "]];
+var snake;
+
+var funthing = (m) => {players[m.getInt(0)] = [m.getString(2), m.getString(1), Array(5).fill(false), (([66, 106, 146, 186, 226, 266, 306, 346].includes(m.getDouble(4)/16)) && ([16, 45].includes(m.getDouble(5)/16)))];};
 try {
 clearTimeout(cTime);
 } catch(err) {var cTime;}
@@ -48,25 +114,24 @@ connection.addMessageCallback("*", function(m) {
 			playerx = 17;
 			playery = 17;
 			Move(playerx, playery);
-			snake = new SnakeGame("dead");
 			connection.send("smiley", 64);
 			//connection.send("say", "hi guys im here");
 		} break;
 			
+		case "add": {
+		    //setTimeout(() => {connection.send("say", "/givegod " + m.getString(1) + "");}, 1000);
+		    funthing(m);
+		} break;
 		
 		case "say": {
 			if (!(/^[.!?]/.test(m.getString(1)))) break; // you can easily add more supported prefixes by changing the regex
 			
 			command = m.getString(1).slice(1).toLowerCase().split(" ");
 			switch (command[0]) {
-				case "help": { // .help [something]
-					switch (command[1]) {
-                        default: { // .help
-                            connection.send("pm", m.getInt(0), "i cannot help you");
-                        } break;
-                    }
+				case "help": { // .help
+                    connection.send("pm", m.getInt(0), "i cannot help you"); // i thought this was funny
                 } break;
-				case "game": {
+				/*case "game": {
 				    switch (command[1]){
 				        case "start": {
 				            snake = new SnakeGame([[3, 0], [2, 0], [1, 0], [0, 0]], 10, 10, 170, 70, 3, false);
@@ -74,7 +139,7 @@ connection.addMessageCallback("*", function(m) {
 				            movesQueue = [[0], [1], [2], [3]];
 				        } break;
 				        case "end": {
-				            //snake.end();
+				            snake.end();
 				        } break;
 				        case "refresh": {
 				            snake.rScreen();
@@ -83,30 +148,27 @@ connection.addMessageCallback("*", function(m) {
 				            connection.send("pm", m.getInt(0), "invalid \"game\" command usage. type .help, !help or ?help for commands");
 				        }
 				    }
-				} break;
+				} break;*/ // now automated
 				default: {
 				    connection.send("pm", m.getInt(0), "unknown command. type .help, !help or ?help for commands");
 				}
 			}
 		} break;
 		
-		case "add": {
-		    //setTimeout(() => {connection.send("say", "/givegod " + m.getString(1) + "");}, 1000);
-		    players[m.getInt(0)] = [m.getString(2), m.getString(1), Array(5).fill(false), (([66, 106, 146, 186, 226, 266, 306, 346].includes(m.getDouble(4)/16)) && ([16, 45].includes(m.getDouble(5)/16)))];
-		} break;
+		
 		
 		case "m": {
 		    if (([66, 106, 146, 186, 226, 266, 306, 346].includes(m.getDouble(1)/16)) && ([16, 45].includes(m.getDouble(2)/16))) {
 		        let dir = false;
 		        let vote = (v) => {
 		            if (!(movesQueue[v].slice(1).includes(players[m.getInt(0)][0]))) {
-                        movesQueue[v] = movesQueue[v].concat([players[m.getInt(0)][0]]);
+		                try {
+		                    if (!(snake.dead)) {
+                                movesQueue[v] = movesQueue[v].concat([players[m.getInt(0)][0]]);
+		                    }
+		                } catch(err) {console.log(err)}
                     } else {
-                        try {
-                            movesQueue[v].splice(movesQueue[v].indexOf(players[m.getInt(0)][0]), 1);
-                        } catch(err) {
-                            console.log(err);
-                        }
+                        movesQueue[v].splice(movesQueue[v].indexOf(players[m.getInt(0)][0]), 1);
                     }
 		        };
                 if (m.getInt(7)) {
@@ -123,21 +185,18 @@ connection.addMessageCallback("*", function(m) {
                 let teleY = (movesQueue[3].includes(players[m.getInt(0)][0]) * 29) + 16;
 
 		        if (m.getBoolean(10) || m.getBoolean(9) || players[m.getInt(0)][2][4]) {
-		            console.log("/tp "+players[m.getInt(0)][1]+" 17 24");
 		            connection.send("say", "/tp "+players[m.getInt(0)][1]+" 17 24");
 		            players[m.getInt(0)][3] = false;
 		        } else {
-                    console.log("/tp "+[players[m.getInt(0)][1], teleX, teleY].join(" "));
 		            connection.send("say", "/tp "+[players[m.getInt(0)][1], teleX, teleY].join(" "));
 		            players[m.getInt(0)][3] = true;
 		        }
-		        console.log(movesQueue);
+		        //console.log(movesQueue);
             } else if ([(m.getDouble(1)/16), (m.getDouble(2)/16)].join(" ") == "20 24") {
                 let teleX = (movesQueue[0].includes(players[m.getInt(0)][0]) * 40) +
                             (movesQueue[1].includes(players[m.getInt(0)][0]) * 80) +
                             (movesQueue[2].includes(players[m.getInt(0)][0]) * 160) + 66;
                 let teleY = (movesQueue[3].includes(players[m.getInt(0)][0]) * 29) + 16;
-                console.log("/tp "+[players[m.getInt(0)][1], teleX, teleY].join(" "));
 		        connection.send("say", "/tp "+[players[m.getInt(0)][1], teleX, teleY].join(" "));
 		        players[m.getInt(0)][3] = true;
             }
@@ -187,11 +246,16 @@ class SnakeGame {
         this.fruit = [0, 0];
         this.score = 0;
         this.gx = gx;
+        this.time = 0;
         this.gy = gy;
         this.dead = (snake == "dead");
+        this.energy = 2;
+        if (!this.dead) {
+            //connection.send("say", "a new game has been start");
+        }
         this.scale = (typeof scale == 'undefined') ? 1 : scale;
         this.movingAxis = movingAxis;
-        this.lastVotes = [0, 0, 0, 0]
+        this.lastVotes = [0, 0, 0, 0];
         var isTouching = false;
         do {
             this.fruit = [Math.floor(Math.random()*this.width), Math.floor(Math.random()*this.height)];
@@ -210,35 +274,38 @@ class SnakeGame {
                         let place = [this.fruit].concat(this.snake).findIndex(f);
                         let eyes = [[0, 1], [2, 1]];
                         let h = (this.snake[1][0] == this.snake[0][0]);
-                        place = [tiles.blank, tiles.fruit, ((h) ? tiles.snakeHeadY : tiles.snakeHeadX)][place + 1];
-                        if (typeof place == "undefined") {
-                            place = tiles.snakeBody;
+                        let tile = [tiles.blank, tiles.fruit, ((h) ? tiles.snakeHeadY[0+(this.energy > 0)] : tiles.snakeHeadX[0+(this.energy > 0)]), ...Array(Math.floor(this.energy)).fill(tiles.snakeBody[1])][place + 1];
+                        if (typeof tile == "undefined") {
+                            tile = tiles.snakeBody[0];
                         }
-                        /*switch(place){
-                            case -1:{
-                                place = 1088;
-                                smiley = 141;
-                            } break;
-                            case 0: {
-                                place = 12;
-                                smiley = 110;
-                            } break;
-                            case 1: {
-                                place = 19;
-                                smiley = 143;
-                            } break;
-                            default: {
-                                place = 14;
-                                smiley = 176;
-                            }
-                        }*/
-                        BH.place(0 - (Math.pow(i[0]-this.snake[0][0]*this.scale, 2) + Math.pow(i[1]-this.snake[0][1]*this.scale, 2)), 0, i[0]+this.gx, i[1]+this.gy, place[i[1] % this.scale][i[0] % this.scale]);
+                        try {
+                        BH.place(0 - (Math.pow(i[0]-this.snake[0][0]*this.scale, 2) + Math.pow(i[1]-this.snake[0][1]*this.scale, 2)), 0, i[0]+this.gx, i[1]+this.gy, tile[i[1] % this.scale].codePointAt(i[0] % this.scale) - 32);
+                        } catch(err) {
+                                console.log(err);
+                        }
                     }
                 }
                 for (i = [0, 0, 0]; i[0] < 4; i[0]++) {
                     for (i[1] = 0; i[1] < 60; i[1]++) {
                         for (i[2] = 0; i[2] < 3; i[2]++) {
-                            BH.place(Math.random(), 0, 180 + i[1], 107 + (i[0] * 6) + i[2], ((Math.floor(i[1] / 3) < this.lastVotes[i[0]]) ? 87 : 0));
+                            try {
+                            BH.place(Math.random(), 0, 156 + i[1], 114 + (i[0] * 6) + i[2], ((Math.floor(i[1] / 3) < this.lastVotes[i[0]]) ? 143 : 0));
+                            } catch(err) {
+                                console.log(err);
+                            }
+                        }
+                    }
+                }
+                for (i[0] = 0;i[0] < 2;i[0]++) {
+                    for (i[1] = 0; i[1] < 4; i[1]++) {
+                        for (i[2] = 0; i[2] < 3; i[2]++) {
+                            for (i[3] = 0;i[3] < 3;i[3]++) {
+                                try {
+                                BH.place(Math.random(), 0, 190 + i[3] + (i[1] * 4), 104 + i[2] + (i[0] * 4), tiles.numbers[(((i[0]) ? (this.score) : (this.time))+"    ")[i[1]].replace(/ /, "10")-[]][i[2]].codePointAt(i[3]) - 32);
+                                } catch(err) {
+                                console.log(err);
+                                }
+                            }
                         }
                     }
                 }
@@ -248,7 +315,7 @@ class SnakeGame {
         this.passFrame = function (dir, lastVotes) {
             if (!this.dead) {
                 let snakeNew = this.snake.slice(0);
-                console.log(this.lastVotes = lastVotes);
+                this.lastVotes = lastVotes;
                 isTouching = false;
                 snakeNew.unshift([snakeNew[0][0] + ((dir == 1) - (dir === 0)), snakeNew[0][1] + ((dir == 3) - (dir == 2))]);
                 let deleteThis;
@@ -291,6 +358,7 @@ class SnakeGame {
                 }
                 this.snake = snakeNew.slice(0);
                 this.movingAxis = (dir > 1);
+                this.time += 1;
                 this.rScreen();
                 return deleteThis;
             } else {
@@ -301,10 +369,10 @@ class SnakeGame {
         this.end = function () {
             this.dead = true;
             clearTimeout(cTime);
-            setTimeout(function(){snake = new SnakeGame([[3, 0], [2, 0], [1, 0], [0, 0]], 10, 10, 170, 70, 3, false); setTimeout(doCommand, ratelimit); movesQueue = [[0], [1], [2], [3]];}, 10000);
+            setTimeout(start, 5000);
             for (var i = [0, 0]; i[1] < (this.height * this.scale); i[1]++) {
                 for (i[0] = 0; i[0] < (this.width * this.scale); i[0]++) {
-                    BH.place((Math.pow(i[0]-(this.snake[0][0]*this.scale), 2) + Math.pow(i[1]-(this.snake[0][1]*this.scale), 2)), 0, i[0]+this.gx, i[1]+this.gy, 87);
+                    BH.place((Math.pow(i[0]-(this.snake[0][0]*this.scale), 2) + Math.pow(i[1]-(this.snake[0][1]*this.scale), 2)), 0, i[0]+this.gx, i[1]+this.gy, tiles.blank[1].charCodeAt(0) - 32);
                 }
             }
         };
@@ -317,7 +385,7 @@ doCommand = function () {
         asdf = movesQueue.slice(0).map((a) => {return [a[0], a.length-1];});
         fdsa = [asdf[0][1], asdf[1][1], asdf[2][1], asdf[3][1]];
         asdf.sort((a,b)=>{return (b[1]-a[1]);});
-        if ((asdf[0][1] == asdf[1][1]) || (BH.nextTick !== null && BH.flusher !== null)) {cTime = setTimeout(doCommand, ratelimit / 2); break okay;}
+        if ((asdf[0][1] == asdf[1][1]) || (BH.nextTick !== null && BH.flusher !== null)) {if (!snake.dead) {cTime = setTimeout(doCommand, ratelimit / 2)}; break okay;}
         snake.passFrame(asdf[0][0], fdsa);
         movesQueue = [[0], [1], [2], [3]];
         for (const [key, val] of Object.entries(players)) {
@@ -332,9 +400,10 @@ doCommand = function () {
     }
 };
 
-try {
-    snake = new SnakeGame([[3, 0], [2, 0], [1, 0], [0, 0]], 10, 10, 170, 70, 3, false);
+start = ()=>{try {
+    snake = new SnakeGame([[5, 5], [4, 5]], 10, 10, 170, 70, 3, false);
     setTimeout(doCommand, ratelimit); movesQueue = [[0], [1], [2], [3]];
 } catch(err) {
     console.log(err)
-}
+}};
+setTimeout(start, 5000)
